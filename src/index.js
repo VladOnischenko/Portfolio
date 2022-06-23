f4f3894b5663f33560d1f40e8d3e164a7203b37f
Vlad <vladonish@gmail.com>
2022-06-23 16:48:43 +0300

    Clear Portfolio

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from "redux";
-import { Provider } from 'react-redux'
 import MainComponent from "./components/MainComponent";
-import { rootReducer } from './store/redusers'
-
-const store = createStore(rootReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <MainComponent />
-    </Provider>
   </React.StrictMode>
 );
-
